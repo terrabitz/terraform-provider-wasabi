8fcaae3418fcb6a761d26e370a1795f1b68e5f29
Trevor Taubitz <terrabitz@protonmail.com>
2022-04-16 11:23:47 -0400

    fix: Include tools directory to manage tfplugindocs version
    Signed-off-by: Trevor Taubitz <terrabitz@protonmail.com>

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 
 // Run the docs generation tool, check its repository for more information on how it works and how docs
 // can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@latest
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
